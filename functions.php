8e2f53f51f3d69bb872c03b5beb4a14ae74eadfb
Jure Cuhalev <gandalfar@gmail.com>
2013-07-18 17:38:19 +0200

    first stab at html for inside pages

--- functions.php
+++ functions.php
@@ -41,10 +41,20 @@ function get_slideshow_posts() {
 
 function register_mainsite_sidebars() {
 	register_sidebar( array(
+	   'name' => __( 'General sidebar'),
+	   'id' => 'general-sidebar',
+	   'description' => __( 'General sidebar that is used on most inside pages', 'mainsite' ),
+	   'before_widget' => '<div class="large-12 columns"><aside id="%1$s" class="widget %2$s">',
+	   'after_widget' => "</aside></div>",
+	   'before_title' => '<h3 class="footer-title">',
+	   'after_title' => '</h3>',
+	) );
+
+	register_sidebar( array(
 	   'name' => __( 'Footer'),
 	   'id' => 'footer',
 	   'description' => __( 'Footer area', 'mainsite' ),
-	   'before_widget' => '<div class="small-3 columns"><aside id="%1$s" class="widget %2$s">',
+	   'before_widget' => '<div class="large-3 columns"><aside id="%1$s" class="widget %2$s">',
 	   'after_widget' => "</aside></div>",
 	   'before_title' => '<h3 class="footer-title">',
 	   'after_title' => '</h3>',
--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php get_header() ?>
 
+
+<div class="row">
+	<div class="large-3 columns">
+		<?php dynamic_sidebar('general-sidebar'); ?>
+	</div>
+
+	<div class="large-9 columns">
+		<?php 
+			while ( have_posts() ) {
+				the_post();
+				get_template_part('content', get_post_format());
+			}
+		?>
+	</div>
+</div>	
+
 <?php get_footer() ?>
