e2ebab366e566ba10fa32f39909a5e05a0c54e65
Jure Cuhalev <gandalfar@gmail.com>
2013-08-17 16:21:53 +0200

    page with styles

---
+++ page-styles.php
@@ -0,0 +1,93 @@
+<?php 
+	Template: Styles
+?>
+<?php get_header(); ?>
+<div class="row">
+	<div class="large-3 columns">
+		<ul class="page-sidebar">
+			    <li class="page_item page-item-2365 current_page_ancestor current_page_parent"><a href="#">About OCWC</a></li>
+			    <li class="page_item page-item-2396 current_page_item"><a href="#">Board of Director</a></li>
+				<li class="page_item page-item-2399"><a href="#">Member listing</a></li>
+				<li class="page_item page-item-2393"><a href="#">Staff</a></li>
+			</ul>		
+	</div>
+	<div class="large-9 columns">
+		<h1>H1 - Main title size</h1>
+		<h2>H2 - Second title size</h2>
+		<h3>H3 - Third title size</h3>
+		<h4>H4 - Fourth title size</h4>
+
+		<p>Example page listing sidebar on the left</p>
+
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
+</div>
+
+<div class="row">
+	<div class="large-3 columns">
+	</div>
+	<div class="large-9 columns">
+		<h2>Basic content styles</h2>
+
+		<pre>Image shoulde be aligned left and flow with the text</pre>
+
+		<p style="text-align: left;" align="center">These webinars presented earlier this year feature OER leaders on issues affecting community colleges. Youtube <a href="http://www.ocwconsortium.org/blog/2013/07/01/cccoer-2013-webinars-available-on-youtube-and-slideshare/cccoer-ocw-new/" rel="attachment wp-att-1563"><img class="alignright  wp-image-1563" title="CCCOER-OCW" src="http://www.ocwconsortium.org/components/com_wordpress/wp/wp-content/uploads/2013/07/CCCOER-OCW-New-300x186.jpg" alt="CCCOER logo" width="180" height="112"></a>archives and slideshare downloads are available for you to review and share with colleagues.&nbsp; CCCOER would like to thank the educators who shared their expertise with our community and all of our attendees who participated this last year.</p>
+	</div>
+</div>
+
+<div class="row">
+	<div class="large-3 columns">
+	</div>
+	<div class="large-9 columns">
+		<h2>Search results</h2>
+
+		<table>
+			<thead>
+				<tr>
+					<th>Course Title</th>
+					<th>Details</th>
+					<th>Source</th>
+					<th>Language</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td><a href="http://openlearn.open.ac.uk/course/view.php?name=MU120_1" target="_blank">Maths everywhere</a></td>
+					<td><a href="/courses/view/e198c6ab62a80f2f0418a38c7b665438/" target="_blank">Details</a></td>
+					<td>The Open University</td>
+					<td>English</td>
+				</tr>
+				<tr>
+					<td><a href="http://ocw.njit.edu/csla/math/math-111/index.php" target="_blank">Math 111:  Calculus</a></td>
+					<td><a href="/courses/view/33e544f248d9f37887b1c0fd9a28b940/" target="_blank">Details</a></td>
+					<td>New Jersey Institute of Technology</td>
+					<td>English</td>
+				</tr>
+							<tr>
+					<td><a href="http://openlearn.open.ac.uk/course/view.php?name=S151_1" target="_blank">Maths for Science</a></td>
+					<td><a href="/courses/view/90faf01da70acb66a9bc72911b5d3d7a/" target="_blank">Details</a></td>
+					<td>The Open University</td>
+					<td>English</td>
+				</tr>
+							<tr>
+					<td><a href="http://ocw.capilanou.ca/mathematics-statistics/math-116-calculus-i-2" target="_blank">Math 116 - Calculus I - and a really long title that streches for two lines at least and some more in some cases.</a></td>
+					<td><a href="/courses/view/e9b5ad577dbec1e79bad0e3e6d9c1a0e/" target="_blank">Details</a></td>
+					<td>Capilano University</td>
+					<td>English</td>
+				</tr>
+				<tr>
+					<td><a href="http://ocw.capilanou.ca/mathematics-statistics/math-116-calculus-i" target="_blank">MATH 116 - Calculus I</a></td>
+					<td><a href="/courses/view/35c84928836507d33eb0817d15b9a21a/" target="_blank">Details</a></td>
+					<td>Capilano University</td>
+					<td>English</td>
+				</tr>
+			</tbody>
+		</table>		
+	</div>
+</div>
+<?php get_footer(); ?>
