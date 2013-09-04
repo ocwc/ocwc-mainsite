e80800fe2aa24c432520dcb571c35998582e996b
OCWC Tech <tech@ocwconsortium.org>
2013-09-04 10:59:18 -0400

    rename query var to search from q

--- courses/course_search.php
+++ courses/course_search.php
@@ -1,4 +1,4 @@
 	<form method="GET" action="/courses/search/" class="">
-		<input type="text" placeholder="Search for courses" name="q" <?php if ( get_query_var('q') ) ?>value="<?php echo get_query_var('q'); ?>">
+		<input type="text" placeholder="Search for courses" name="search" <?php if ( get_query_var('search') ) ?>value="<?php echo get_query_var('search'); ?>">
 		<button type="submit" class="button">Search</button>
 	</form>
