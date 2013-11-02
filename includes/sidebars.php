3ea83c38579f0ed0b12930fe441a911da96e6299
Jure Cuhalev <gandalfar@gmail.com>
2013-11-02 13:45:30 +0100

    move sidebar into its own file and added custom walker for pages

--- includes/sidebars.php
+++ includes/sidebars.php
@@ -47,4 +47,54 @@ function register_mainsite_nav_menu() {
   register_nav_menu('navigation-menu',__( 'Navigation Menu' ));
 }
 
+class PageTitleWalker extends Walker_page {
+	function start_el( &$output, $page, $depth = 0, $args = array(), $current_page = 0 ) {
+		if ( $depth )
+			$indent = str_repeat("\t", $depth);
+		else
+			$indent = '';
+
+		extract($args, EXTR_SKIP);
+		$css_class = array('page_item', 'page-item-'.$page->ID);
+
+		if ( get_field('sidebar_hide', $page->ID) ) {
+			return;
+		}
+
+		if ( !empty($current_page) ) {
+			$_current_page = get_post( $current_page );
+			if ( in_array( $page->ID, $_current_page->ancestors ) )
+				$css_class[] = 'current_page_ancestor';
+			if ( $page->ID == $current_page )
+				$css_class[] = 'current_page_item';
+			elseif ( $_current_page && $page->ID == $_current_page->post_parent )
+				$css_class[] = 'current_page_parent';
+		} elseif ( $page->ID == get_option('page_for_posts') ) {
+			$css_class[] = 'current_page_parent';
+		}
+
+		$css_class = implode( ' ', apply_filters( 'page_css_class', $css_class, $page, $depth, $args, $current_page ) );
+
+		if ( '' === $page->post_title )
+			$page->post_title = sprintf( __( '#%d (no title)' ), $page->ID );
+
+		if ( get_field('sidebar_label', $page->ID) ) {
+			$title = get_field('sidebar_label', $page->ID);
+		} else {
+			$title = apply_filters( 'the_title', $page->post_title, $page->ID );
+		}
+
+		$output .= $indent . '<li class="' . $css_class . '"><a href="' . get_permalink($page->ID) . '">' . $link_before . $title . $link_after . '</a>';
+
+		if ( !empty($show_date) ) {
+			if ( 'modified' == $show_date )
+				$time = $page->post_modified;
+			else
+				$time = $page->post_date;
+
+			$output .= " " . mysql2date($date_format, $time);
+		}
+	}
+}
+
 ?>
---
+++ partials/sidebar.php
@@ -0,0 +1,5 @@
+<?php $walker = new PageTitleWalker(); ?>
+<?php wp_list_pages( array('title_li'=>'','include'=>get_post_top_ancestor_id()) ); ?>
+<?php wp_list_pages( array('title_li'=>'','depth'=>3,'child_of'=>get_post_top_ancestor_id(),'walker' => $walker) ); ?>
+
+<?php //dynamic_sidebar('sidebar-general'); ?>
