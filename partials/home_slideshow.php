5fddcf313dbf809bc0795a42e43c821ebc5cc344
Jure Cuhalev <gandalfar@gmail.com>
2013-11-04 20:48:44 +0100

    support for empty slideshows on front page, also slider buttons

--- partials/home_slideshow.php
+++ partials/home_slideshow.php
@@ -3,24 +3,34 @@
 	$slideshow_posts = get_slideshow_posts(); 
 ?>
 
-<div class="rslides">
-<?php while ( $slideshow_posts->have_posts() ) : $slideshow_posts->the_post(); ?>
-	<?php if ( get_field('slideshow_background') ) : ?>
-		<div class="slideshow" style="background-color: <?php the_field('slideshow_background'); ?>;">
-			<div class="row">
-				<?php if ( get_field('slideshow_image') ) : ?>
-					<li class="slide"><div class="card" style="<?php echo get_box_background(get_field('slideshow_background'), 0.9); ?>"><h1><a href="<?php the_field('slideshow_url'); ?>"><? the_title(); ?></a></h1><p><?php the_field('slideshow_description'); ?></p></div><div class="overlay" style="<?php the_linear_gradient(get_field('slideshow_background')); ?>"></div><img src="<?php echo get_field('slideshow_image')['sizes']['slideshow-image-large']; ?>" /></li>
-				<?php endif; ?>
+<div class="rslides-wrapper">
+	<div class="rslides">
+	<?php while ( $slideshow_posts->have_posts() ) : $slideshow_posts->the_post(); ?>
+		<?php if ( get_field('slideshow_background') AND strlen(get_field('slideshow_description')) > 10 ) : ?>
+			<div class="slideshow" style="background-color: <?php the_field('slideshow_background'); ?>;">
+				<div class="row">
+					<?php if ( get_field('slideshow_image') ) : ?>
+						<li data-attr="1" class="slide"><div class="card" style="<?php echo get_box_background(get_field('slideshow_background'), 0.9); ?>"><h1><a href="<?php the_field('slideshow_url'); ?>"><? the_title(); ?></a></h1><p><?php the_field('slideshow_description'); ?></p></div><div class="overlay" style="<?php the_linear_gradient(get_field('slideshow_background')); ?>"></div><img src="<?php echo get_field('slideshow_image')['sizes']['slideshow-image-large']; ?>" /></li>
+					<?php endif; ?>
+				</div>
 			</div>
-		</div>
-	<?php else : ?>
-		<div class="slideshow">
-			<div class="row">
-				<?php if ( get_field('slideshow_image') ) : ?>
-					<a href="<?php the_field('slideshow_url'); ?>"><img src="<?php echo get_field('slideshow_image')['sizes']['slideshow-image-large']; ?>" /></a>
-				<?php endif; ?>
+		<?php elseif ( get_field('slideshow_background') ) : ?>
+			<div class="slideshow" style="background-color: <?php the_field('slideshow_background'); ?>;">
+				<div class="row">
+					<?php if ( get_field('slideshow_image') ) : ?>
+						<li data-attr="2" class="slide"><a href="<?php the_field('slideshow_url'); ?>"><div class="overlay" style="<?php the_linear_gradient(get_field('slideshow_background')); ?>"></div></a><img src="<?php echo get_field('slideshow_image')['sizes']['slideshow-image-large']; ?>" /></li>
+					<?php endif; ?>
+				</div>
 			</div>
-		</div>
-	<?php endif; ?>
-<?php endwhile; ?>
+		<?php else : ?>
+			<div class="slideshow">
+				<div class="row">
+					<?php if ( get_field('slideshow_image') ) : ?>
+						<a data-attr="3" href="<?php the_field('slideshow_url'); ?>"><img src="<?php echo get_field('slideshow_image')['sizes']['slideshow-image-large']; ?>" /></a>
+					<?php endif; ?>
+				</div>
+			</div>
+		<?php endif; ?>
+	<?php endwhile; ?>
+	</div>
 </div>
