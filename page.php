2937f70a429ff912e5957457a85f293eb5605eb9
Jure Cuhalev <gandalfar@gmail.com>
2013-11-04 17:42:49 +0100

    header image stub

--- page.php
+++ page.php
@@ -1,23 +1,19 @@
 <?php get_header() ?>
 
-<div class="row main-wrapper">
-	<?php if ( have_posts() ) : ?>	
-		<?php while ( have_posts() ) : the_post(); ?>
-
-		<div class="large-12 columns">
-			<?php get_template_part('partials/header_image', get_post_type()); ?>
+<?php if ( have_posts() ) : ?>	
+	<?php while ( have_posts() ) : the_post(); ?>
+		<?php get_template_part('partials/header_image', get_post_type()); ?>
+		<div class="row main-wrapper">
+			<div class="large-3 columns">
+				<?php get_template_part('partials/sidebar', get_post_type()); ?>
+			</div>
+			<div class="large-9 columns">
+					<?php get_template_part('partials/content', get_post_type()); ?>
+			</div>
 		</div>
-
-		<div class="large-3 columns">
-			<?php get_template_part('partials/sidebar', get_post_type()); ?>
-		</div>
-		<div class="large-9 columns">
-				<?php get_template_part('partials/content', get_post_type()); ?>
-		</div>
-		<?php endwhile; ?>
-	<?php else : ?>
-		<h1>404 Not found</h1>
-	<?php endif; ?>
-</div>	
+	<?php endwhile; ?>
+<?php else : ?>
+	<h1>404 Not found</h1>
+<?php endif; ?>
 
 <?php get_footer() ?>
--- partials/header_image.php
+++ partials/header_image.php
@@ -1,6 +1,10 @@
 <?php $header_image = mainsite_get_closest_header_image(); ?>
 <?php if ($header_image) : ?>
-
-	<img src="<?php echo $header_image['sizes']['large']; ?>" />
-
+<div class="header-image-wrapper" style="background: <?php echo the_field('header_image_background_color'); ?>">
+	<div class="row">
+		<div class="large-12 columns">
+			<img src="<?php echo $header_image['sizes']['header-image']; ?>" />
+		</div>
+	</div>
+</div>
 <?php endif;?>
