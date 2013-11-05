f11d7d3bc84c532ab3cbe2ed75c99fa426428eeb
Jure Cuhalev <gandalfar@gmail.com>
2013-11-05 16:06:16 +0100

    only show author byline for posts

--- partials/content-post.php
+++ partials/content-post.php
@@ -1,5 +1,8 @@
 <header class="entry-header">
 	<h1 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+	<div class="entry-meta">
+		<p>Posted by <?php echo get_the_author(); ?> on <?php the_time('l, F jS, Y') ?></p>
+	</div>
 </header>
 <div class="entry-content">
 	<?php the_content(); ?>
