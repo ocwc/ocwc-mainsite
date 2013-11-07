28f0800d58105df212dc6974d2ec633abe3298e6
Jure Cuhalev <gandalfar@gmail.com>
2013-11-07 22:23:33 +0100

    Add Google Analytics

--- footer.php
+++ footer.php
@@ -4,6 +4,8 @@
   	</div>
 </div>
 
-	<?php wp_footer(); ?>
+<?php wp_footer(); ?>
+<script type="text/javascript">var _gaq=_gaq||[];_gaq.push(['_setAccount','UA-4248822-3']);_gaq.push(['_trackPageview']);(function(){var ga=document.createElement('script');ga.type='text/javascript';ga.async=true;ga.src=('https:'==document.location.protocol?'https://ssl':'http://www')+'.google-analytics.com/ga.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(ga,s);})();</script>
+
 </body>
 </html>
