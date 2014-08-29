8d26cf304824bd2214469dc864f397cf665e39ca
Jure Cuhalev <gandalfar@gmail.com>
2014-08-29 19:06:08 +0200

    ability to expose our header and footer through API - django magic ;)

--- footer.php
+++ footer.php
@@ -1,12 +1,6 @@
-<div class="footer-wrapper">
-	<div class="container">
-		<?php dynamic_sidebar('footer'); ?>
-		<?php dynamic_sidebar('footer-wide'); ?>
-  	</div>
-</div>
+<?php include('pages/page-api-footer.php'); ?>
 
 <?php wp_footer(); ?>
-<script type="text/javascript">var _gaq=_gaq||[];_gaq.push(['_setAccount','UA-4248822-3']);_gaq.push(['_trackPageview']);(function(){var ga=document.createElement('script');ga.type='text/javascript';ga.async=true;ga.src=('https:'==document.location.protocol?'https://ssl':'http://www')+'.google-analytics.com/ga.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(ga,s);})();</script>
 
 </body>
 </html>
---
+++ pages/page-api-header.php
@@ -0,0 +1,60 @@
+<?php 
+/*
+	Template name: API - Header
+*/
+?>
+<div class="header-container-wrapper">
+		<div class="header-row">
+			<div class="container">
+				<div class="show-for-xs-up col-sm-3 col-md-2 pull-right text-center tab members">
+					<a href="http://members.oeconsortium.org" class="member-portal">Members Portal</a>
+				</div>
+
+				<div class="show-for-xs-up col-sm-3 col-md-2 pull-right tab translation">
+					<?php get_template_part('partials/translate_widget'); ?>
+				</div>
+			</div>
+		</div>
+	
+	<div class="container">
+		<div class="col-xs-12 col-sm-4 pull-left">
+			<a href="/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo.png" alt="The Open Education Consortium" class="logo" /></a>
+		</div>
+		<div class="show-for-xs-up col-sm-4 col-md-3 pull-right search-form-row">
+			<form class="header-search" method="GET" action="/courses/search/">
+				<input placeholder="Search for open courses ..." name="search">
+				<i class="icon-search header-search-icon"></i>
+			</form>
+		</div>
+	</div>
+
+	<div class="container">
+		<nav class="navbar navbar-default" role="navigation">
+			<div class="navbar-header visible-xs-block">
+				<form class="header-search" method="GET" action="/courses/search/">
+					<input placeholder="Search for open courses ..." name="search">
+				</form>
+
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-menu">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+			</div>
+			<?php 
+	            wp_nav_menu( array(
+	                'menu'              => 'navigation-menu',
+	                'theme_location'    => 'navigation-menu',
+	                'depth'             => 2,
+	                'container'         => 'div',
+	                'container_class'   => 'collapse navbar-collapse',
+	        		'container_id'      => 'navigation-menu',
+	                'menu_class'        => 'nav navbar-nav',
+	                'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+	                'walker'            => new wp_bootstrap_navwalker())
+	            );
+	        ?>
+		</nav>
+	</div>
+</div>
