fe19b185e4dfadab6309bddbd282575e8ed78716
Jure Cuhalev <gandalfar@gmail.com>
2015-02-03 23:51:16 +0100

    added openlearning to courses sidebar

---
+++ sidebar-courses.php
@@ -0,0 +1,6 @@
+<?php dynamic_sidebar('sidebar-courses'); ?>
+
+<br /><br />
+<a href="http://www.openlearningportfolio.com/" target="_blank" title="OpenPortfolio">
+	<img src="http://www.oeconsortium.org/wp-content/uploads/2015/02/openportfolio.jpeg" alt="OpenPortfolio" />
+</a>
