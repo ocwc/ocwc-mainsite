e84123463d1b1807f2a04ce515f513ccbea884d8
Jure Cuhalev <gandalfar@gmail.com>
2015-09-09 14:42:16 +0200

    Infocard websites screenshots

--- partials/_resource_item.php
+++ partials/_resource_item.php
@@ -1,6 +1,6 @@
 <?php global $resource; ?>
 
-<?php 
+<?php
 	$source_type = 'web';
 
 	if ( strpos($resource['url'], 'youtube') ) {
@@ -13,12 +13,20 @@
 ?>
 
 <div class="col-sm-4 infomap-resource-wrapper">
-	<div 
+	<div
 		class="infomap-resource <?php echo $source_type; ?>"
-		<?php if ( $source_type === 'youtube' ) : ?>style="background-image: url('http://img.youtube.com/vi/<?php echo $youtube_id; ?>/0.jpg');" <?php endif; ?>
+		<?php if ( $source_type === 'youtube' ) : ?>
+            style="background-image: url('http://img.youtube.com/vi/<?php echo $youtube_id; ?>/0.jpg');"
+        <?php else : ?>
+            <?php
+                $url_stripped = str_replace('https://', '', $resource['url']);
+                $url_stripped = str_replace('http://', '', $url_stripped);
+            ?>
+            style="background-image: url('http://s.wordpress.com/mshots/v1/<?php echo $url_stripped; ?>?w=400'); background-position-y: -25px;"
+        <?php endif; ?>
 		>
-		<a class="infomap-resource-title" 
-		   href="<?php echo $resource['url']; ?>" 
+		<a class="infomap-resource-title"
+		   href="<?php echo $resource['url']; ?>"
 		   title="<?php echo $resource['title']; ?>"
 		   target="_blank">
 		   	<?php echo $resource['title']; ?>
