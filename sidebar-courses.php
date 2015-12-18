a58ecd808d397540dd105cacce30c831d00a252f
Jure Cuhalev <gandalfar@gmail.com>
2015-12-18 15:31:30 +0100

    Main site

--- sidebar-courses.php
+++ sidebar-courses.php
@@ -1,6 +1,1 @@
 <?php dynamic_sidebar('sidebar-courses'); ?>
-
-<br /><br />
-<a href="http://www.openlearningportfolio.com/" target="_blank" title="OpenPortfolio">
-	<img src="http://www.oeconsortium.org/wp-content/uploads/2015/02/openportfolio.jpeg" alt="OpenPortfolio" />
-</a>
