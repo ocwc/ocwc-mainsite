9900665fc74d36107d9d07adae7dd061430f07c8
Jure Cuhalev <jure@cuhalev.com>
2018-06-26 09:56:51 +0200

    One more mobile tweak for new homepage

--- partials/_home_newsletter.php
+++ partials/_home_newsletter.php
@@ -12,7 +12,7 @@
               action="http://ocwconsortium.us1.list-manage.com/subscribe/post?u=1a209e9010b861e3abfd5c609&amp;id=4e4d1b9eba"
               method="post" name="mc-embedded-subscribe-form" novalidate="" target="_blank">
             <div class="form-group">
-                <div class="col-sm-10"><input id="mce-EMAIL" class="form-control required email" name="EMAIL"
+                <div class="col-xs-8 col-sm-10"><input id="mce-EMAIL" class="form-control required email" name="EMAIL"
                                               type="email" value="" placeholder="Email Address"/></div>
                 <input id="mc-embedded-subscribe" class="btn btn-success" name="subscribe"
                        type="submit" value="Subscribe"/>
