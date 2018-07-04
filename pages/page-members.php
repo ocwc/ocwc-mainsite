c0b2bff6f4d2c5d4aa75e59b4107f39909b2312e
Jure Cuhalev <jure@cuhalev.com>
2018-07-04 12:45:59 +0200

    Update page members

--- pages/page-members.php
+++ pages/page-members.php
@@ -3,28 +3,6 @@
 	Template name: Page Members
 */
 
-function mainsite_members_custom_head() { 
-	?>
-	<link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.6.4/leaflet.css" />
-	<!--[if lte IE 8]>
-		<link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.6.4/leaflet.ie.css" />
-	<![endif]-->
-	<script src="http://cdn.leafletjs.com/leaflet-0.6.4/leaflet.js"></script>
-
-	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/lib/javascripts/plugins/Leaflet.markercluster/MarkerCluster.css" />
-	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/lib/javascripts/plugins/Leaflet.markercluster/MarkerCluster.Default.css" />
-	<!--[if lte IE 8]><link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/lib/javascripts/plugins/Leaflet.markercluster/MarkerCluster.Default.ie.css" /><![endif]-->
-	<script src="<?php echo get_stylesheet_directory_uri(); ?>/lib/javascripts/plugins/Leaflet.markercluster/leaflet.markercluster-src.js"></script>
-
-	<link href="<?php echo get_stylesheet_directory_uri(); ?>/lib/stylesheets/map.css" media="screen, projector, print" rel="stylesheet" type="text/css" />
-	<script src="<?php echo get_stylesheet_directory_uri(); ?>/lib/javascripts/map.js"></script>
-
-	<script type="text/javascript">
-		var members_site_domain = '<?php echo MEMBERS_API_URL; ?>';
-	</script>
-<?php
-}
-add_action('wp_head', 'mainsite_members_custom_head');
 ?>
 
 <?php get_header(); ?>
