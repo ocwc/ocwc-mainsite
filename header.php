8d37f8348dccd56a7e729a561881540599488bf8
Jure Cuhalev <jure@cuhalev.com>
2018-12-21 14:17:17 +0100

    Remove hotjar

--- header.php
+++ header.php
@@ -9,17 +9,6 @@
 
 	<meta name="google-translate-customization" content="d3bf6f511902d7f2-750e02196618cb43-gadc8982319c58076-15">
   	<?php wp_head(); ?>
-    <!-- Hotjar Tracking Code for https://www.oeconsortium.org -->
-    <script>
-        (function(h,o,t,j,a,r){
-            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:1066667,hjsv:6};
-            a=o.getElementsByTagName('head')[0];
-            r=o.createElement('script');r.async=1;
-            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-            a.appendChild(r);
-        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-    </script>
 </head>
 <body <?php body_class(); ?> >
 	<?php include('pages/page-api-header.php'); ?>
