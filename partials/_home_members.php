310e7b348e805851b09dc1e0d2565473891b3a6d
Jure Cuhalev <jure@cuhalev.com>
2019-01-13 10:30:14 +0100

    Support linking to pages of our supporting members

--- partials/_home_members.php
+++ partials/_home_members.php
@@ -6,7 +6,12 @@
     <p class="text-justify">
     <?php while ( have_rows( 'home_supporters', 'option' ) ) : the_row(); ?>
         <?php $image = get_sub_field( 'image' )['sizes']['medium']; ?>
-        <img src="<?= $image ?>" alt="" class="home-members__logo" />
+        <?php if (get_sub_field('link')) : ?>
+            <a href="<?= get_sub_Field('link'); ?>"><img src="<?= $image ?>" alt="" class="home-members__logo" /></a>
+        <?php else : ?>
+            <img src="<?= $image ?>" alt="" class="home-members__logo" />
+        <?php endif; ?>
+
     <?php endwhile; ?>
     </p>
 </div>
