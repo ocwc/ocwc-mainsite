a7756c838dc7395c3fd981389f5a1f386aaafec5
Jure Cuhalev <jure@cuhalev.com>
2019-06-21 16:55:17 +0200

    Fix form for 2019 version

--- pages/page-ace-form-results.php
+++ pages/page-ace-form-results.php
@@ -6,53 +6,51 @@
 <?php get_header(); ?>
 
 <?php
-    $form = GFFormsModel::get_form_meta( 16 );
-    $entries = array();
-    $values = array();
+$form    = GFFormsModel::get_form_meta( 16 );
+$entries = array();
+$values  = array();
 
-    $paging = array('offset' => 0, 'page_size' => 200 );
+$paging = array( 'offset' => 0, 'page_size' => 200 );
 
-    foreach ( GFAPI::get_entries( 15, null, null, $paging ) as $entry) {
-        foreach( $form['fields'] as $field ) {
+foreach ( GFAPI::get_entries( 16, null, null, $paging ) as $entry ) {
+    foreach ( $form['fields'] as $field ) {
 
-            if ( $field['id'] === 1 ) {
+        if ( $field['id'] === 1 ) {
 
-                $values[$field['id']] = array(
-                    'id'    => $field['id'],
-                    'label' => "Nominator's Name",
-                    'value' => $entry['1.3'] . ' ' . $entry['1.6'],
-                );
+            $values[ $field['id'] ] = array(
+                'id'    => $field['id'],
+                'label' => "Nominator's Name",
+                'value' => $entry['1.3'] . ' ' . $entry['1.6'],
+            );
 
-            } elseif ( $field['id'] === 5 ) {
+        } elseif ( $field['id'] === 5 ) {
 
-                $values[$field['id']] = array(
-                    'id'    => $field['id'],
-                    'label' => "Nominee's Name",
-                    'value' => $entry["5.3"] . " " . $entry["5.6"],
-                );
+            $values[ $field['id'] ] = array(
+                'id'    => $field['id'],
+                'label' => "Nominee's Name",
+                'value' => $entry["5.3"] . " " . $entry["5.6"],
+            );
 
-            } else {
+        } else {
 
-                $values[$field['id']] = array(
-                    'id'    => $field['id'],
-                    'label' => $field['label'],
-                    'value' => $entry[ $field['id'] ],
-                );
-
-            }
-
-            $values[$field['id']]['type'] = $field['type'];
+            $values[ $field['id'] ] = array(
+                'id'    => $field['id'],
+                'label' => $field['label'],
+                'value' => $entry[ $field['id'] ],
+            );
 
         }
 
-        $entries[] = array(
-                    'id' => $entry['id'],
-                    'values' => $values
-                    );
+        $values[ $field['id'] ]['type'] = $field['type'];
 
     }
 
-//    var_dump($entries);
+    $entries[] = array(
+        'id'     => $entry['id'],
+        'values' => $values
+    );
+
+}
 ?>
 
 <?php if ( have_posts() ) : ?>
@@ -61,7 +59,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-sm-12">
-                        <?php get_template_part('partials/content', get_post_type()); ?>
+                        <?php get_template_part( 'partials/content', get_post_type() ); ?>
                     </div>
                 </div>
             </div>
@@ -72,37 +70,41 @@
                         <?php $categories = array(
 //                                'INDIVIDUAL AWARDS',
 //                                'STUDENT AWARD',
-                                'RESOURCES / TOOLS / PRACTICES AWARDS'
-                            );
+//                                'RESOURCES / TOOLS / PRACTICES AWARDS'
+                            'OPEN EDUCATIONAL RESOURCES (OER)',
+                            'TOOLS & PRACTICES',
+                        );
                         ?>
 
-                        <?php foreach ($categories as $category) : ?>
+                        <?php foreach ( $categories as $category ) : ?>
                             <h1><?php echo $category; ?></h1>
 
-                            <?php foreach ($entries as $entry) : ?>
+                            <?php foreach ( $entries as $entry ) : ?>
 
                                 <?php if ( $entry['values'][46]['value'] === $category ) : ?>
-                                    <h2 id="<?php echo 'form-' . $entry['id']; ?>"><a href="#<?php echo 'form-' . $entry['id']; ?>">Submission #<?php echo $entry['id']; ?> (<?php echo $category; ?>)</a></h2>
+                                    <h2 id="<?php echo 'form-' . $entry['id']; ?>"><a
+                                            href="#<?php echo 'form-' . $entry['id']; ?>">Submission
+                                            #<?php echo $entry['id']; ?> (<?php echo $category; ?>)</a></h2>
 
                                     <dl>
-                                    <?php foreach ($entry['values'] as $field) : ?>
-                                        <?php if ( $field['type'] == 'section' ) : ?>
-                                            <h3><?php echo $field['label'] ?></h3>
-                                        <?php elseif ( $field['type'] == 'fileupload' ) : ?>
-                                            <dt><?php echo $field['label'] ?></dt>
-                                            <dd>
-                                                <?php $link = trim(stripcslashes($field['value']), '[]"'); ?>
-                                                <a href="<?php echo $link; ?>"><?php echo $link; ?></a>
-                                            </dd>
-                                        <?php else : ?>
-                                            <?php if ( $field['value'] ) : ?>
+                                        <?php foreach ( $entry['values'] as $field ) : ?>
+                                            <?php if ( $field['type'] == 'section' ) : ?>
+                                                <h3><?php echo $field['label'] ?></h3>
+                                            <?php elseif ( $field['type'] == 'fileupload' ) : ?>
                                                 <dt><?php echo $field['label'] ?></dt>
-                                                <dd><?php echo $field['value'] ?></dd>
+                                                <dd>
+                                                    <?php $link = trim( stripcslashes( $field['value'] ), '[]"' ); ?>
+                                                    <a href="<?php echo $link; ?>"><?php echo $link; ?></a>
+                                                </dd>
+                                            <?php else : ?>
+                                                <?php if ( $field['value'] ) : ?>
+                                                    <dt><?php echo $field['label'] ?></dt>
+                                                    <dd><?php echo $field['value'] ?></dd>
+                                                <?php endif; ?>
                                             <?php endif; ?>
-                                        <?php endif; ?>
-                                    <?php endforeach; ?>
+                                        <?php endforeach; ?>
                                     </dl>
-                                    <hr />
+                                    <hr/>
                                 <?php endif; ?>
                             <?php endforeach; ?>
 
